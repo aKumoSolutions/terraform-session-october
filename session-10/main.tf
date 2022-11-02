a33069d9b0d4d7caf1e34f2e3a27045bf9f067be
EC2 Default User <ec2-user@ip-172-31-15-156.us-west-2.compute.internal>
2022-11-02 00:46:53 +0000

    session-10 is added

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,8 +1,13 @@
 module "ec2" {
-  source        = "github.com/aKumoSolutions/terraform-session-october//modules/ec2"
+  source        = "github.com/aKumoSolutions/terraform-session-october//modules/ec2?ref=v1.0.0"
   env           = "dev"
   instance_type = "t2.micro"
   ami           = "ami-0c2d06d50ce30b442"
+
+}
+module "vpc_example_simple-vpc" {
+  source  = "terraform-aws-modules/vpc/aws//examples/simple-vpc"
+  version = "3.18.1"
 }
 
 // github.com = Main domain (SCM)
@@ -12,3 +17,8 @@ module "ec2" {
 // main = Branch
 // modules = Directory
 // ec2 = Subdirectory
+
+// 1. Hashicorp Packer is a tool to build AMIs = check
+// 2. For loop, For Expressions = check
+// 3. For each, meta argument like a count = check
+// 4. Dynamic Resource Creation = check
